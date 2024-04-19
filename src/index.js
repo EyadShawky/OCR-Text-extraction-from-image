aeb45e6c11c9f88d2130b6298be0b1f0119a0af6
EyadShawky <eyadshawky0@gmail.com>
2024-04-19 17:58:16 +0200

    added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './Pages/Home';
+import Home from './Pages/Home'; 
 
 const route = createBrowserRouter([
   { path: '/', element: <Layout />, children:[
